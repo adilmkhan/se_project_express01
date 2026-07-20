b097a1686a4c4b5b493b2f8a2737e234b7082068
Adil Khan <adilkhan@Adils-MacBook-Air.local>
2026-07-20 14:17:25 -0500

    score controller refactor

--- controllers/scores.js
+++ controllers/scores.js
@@ -3,9 +3,7 @@ const { articlesWithScores } = require("../services/scoreService");
 module.exports.getScore = (req, res, next) => {
   return articlesWithScores(req.body)
     .then((result) => {
-      res.send({
-        result,
-      });
+      res.send(result);
     })
     .catch(next);
 };
